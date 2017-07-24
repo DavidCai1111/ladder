04dfe24a232a6cebbc74bb0567f4b7aaf812e8bc
David Cai <davidcai1993@yahoo.com>
2017-07-24 15:29:55 +0800

    feat: add constants for server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -10,6 +10,22 @@ use std::env;
 use clap::App;
 use ladder::util::read_file_content;
 
+const idType: i32 = 0;
+const idIP0: i32 = 1;
+const idDmLen: i32 = 1;
+const idDm0: i32 = 2;
+
+const typeIPv4: i32 = 1;
+const typeDm: i32 = 3;
+const typeIPv6: i32 = 4;
+
+const lenIPv4: usize = 6;
+const lenIPv6: usize = 18;
+const lenDmBase: usize = 2;
+const lenHmacSha1: usize = 10;
+
+const addrMask: u8 = 0xf;
+
 #[derive(Debug, Deserialize)]
 struct CargoConfig {
   name: String,
