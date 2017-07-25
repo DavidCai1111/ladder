aa3d8e389fb9d92a44cb4070b2b02eb05206bab1
David Cai <davidcai1993@yahoo.com>
2017-07-25 15:05:52 +0800

    feat: add config.parse

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #![crate_type = "lib"]
 #![crate_name = "ladder"]
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
 pub mod util;
+pub mod config;
 
 pub use self::util::*;
+pub use self::config::*;
