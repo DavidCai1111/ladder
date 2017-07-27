02867377068a164a5a7af6b8e612226b4ea1e98a
David Cai <davidcai1993@yahoo.com>
2017-07-27 15:34:24 +0800

    refactor: optimize config.parse

--- src/config.rs
+++ src/config.rs
@@ -15,19 +15,19 @@ pub fn parse(filename: &str) -> Result<Config, Box<Error>> {
   let mut config: Config = serde_json::from_str(&read_file_content(filename)?)?;
 
   if config.local_addr.is_none() {
-    config.local_addr = Some(String::from("0.0.0.0:6009"))
+    config.local_addr = Some(String::from("0.0.0.0:6009"));
   }
   if config.server_addr.is_none() {
-    config.server_addr = Some(String::from("0.0.0.0:8089"))
+    config.server_addr = Some(String::from("0.0.0.0:8089"));
   }
   if config.password.is_none() {
-    config.password = Some(String::from("ladder-password"))
+    config.password = Some(String::from("ladder-password"));
   }
   if config.method.is_none() {
-    config.method = Some(String::from("aes-128-cfb"))
+    config.method = Some(String::from("aes-128-cfb"));
   }
   if config.timeout.is_none() {
-    config.timeout = Some(300)
+    config.timeout = Some(300);
   }
 
   Ok(config)
