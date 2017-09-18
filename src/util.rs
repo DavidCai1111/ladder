2657f2d756cce75088494fe489d8240879a9a358
David Cai <davidcai1993@yahoo.com>
2017-09-18 17:12:28 +0800

    refactor: optimize generate_key()

--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,7 @@ use crypto::md5;
 use crypto::digest::Digest;
 use bytes::Bytes;
 
-const MD5_LEN: usize = 16;
+const MD5_LEN: usize = 32;
 
 pub fn read_file_content(filename: &str) -> ioResult<String> {
     let mut file = File::open(filename)?;
@@ -22,10 +22,15 @@ pub fn md5_sum(bytes: &Bytes) -> Bytes {
 }
 
 pub fn generate_key(password: &str, key_len: usize) -> Bytes {
-    // TODO: optimize generate_key()
     let count = (key_len as f64 / MD5_LEN as f64).ceil() as usize;
+    let md5_result = md5_sum(&Bytes::from(password));
+    let mut result = md5_result.clone();
 
-    md5_sum(&Bytes::from(password))
+    for _ in 1..count {
+        result.extend_from_slice(&md5_result);
+    }
+
+    Bytes::from(&result[..key_len])
 }
 
 #[cfg(test)]
@@ -41,11 +46,22 @@ mod tests {
         assert!(read_file_content(cargoTomlPath).unwrap().contains("ladder"));
     }
 
-
     #[test]
     fn test_md5_sum() {
         let result = md5_sum(&Bytes::from("test"));
 
         assert_eq!(result, String::from("098f6bcd4621d373cade4e832627b4f6"));
+    }
+
+    #[test]
+    fn test_generate_key_16() {
+        let mut sum = String::from("5d41402abc4b2a76b9719d911017c592");
+        assert_eq!(generate_key("hello", 32), sum);
+    }
+
+    #[test]
+    fn test_generate_key_32() {
+        let mut sum = String::from("5d41402abc4b2a76b9719d911017c5925d41402abc4b2a76b9719d911017c592");
+        assert_eq!(generate_key("hello", 64), sum);
     }
 }
